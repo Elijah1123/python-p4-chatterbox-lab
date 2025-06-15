c6813f3fdd1b7f2663d68e2330584ed9d03949f2
Elijah M <elijah.macharia@student.moringaschool.com>
2025-06-15 22:43:09 +0300

    Apis

--- server/seed.py
+++ server/seed.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from random import choice as rc
-
 from faker import Faker
 
 from app import app
@@ -9,25 +8,27 @@ from models import db, Message
 
 fake = Faker()
 
-usernames = [fake.first_name() for i in range(4)]
-if "Duane" not in usernames:
-    usernames.append("Duane")
-
 def make_messages():
-
+    print("Deleting old messages...")
     Message.query.delete()
-    
+
+    usernames = [fake.first_name() for _ in range(4)]
+    if "Duane" not in usernames:
+        usernames.append("Duane")
+
+    print("Seeding new messages...")
     messages = []
 
-    for i in range(20):
+    for _ in range(20):
         message = Message(
             body=fake.sentence(),
-            username=rc(usernames),
+            username=rc(usernames)
         )
         messages.append(message)
 
     db.session.add_all(messages)
-    db.session.commit()        
+    db.session.commit()
+    print("Done seeding!")
 
 if __name__ == '__main__':
     with app.app_context():
